f3d5a97a55258dc56d79c5f4c725e020f228f1f5
torequl <torequlmkt@gmail.com>
2024-09-21 02:16:04 +0600

    New Update

--- JS/home.js
+++ JS/home.js
@@ -2,25 +2,18 @@ document.getElementById('add-mony-btn').addEventListener('click', function(event
     event.preventDefault();
 
     const addMoneyInput = document.getElementById('add-money-input').value;
-
+    const addMoneyNumber = parseFloat(addMoneyInput);
     const pinNumberInput = document.getElementById('pin-number-input').value;
 
     if (pinNumberInput === '1234'){
-        console.log('Added Mony')
+        const balance = document.getElementById('balance').innerText;
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        document.getElementById('balance').innerText = newBalance;
+        document.getElementById('add-money-input').value = '';
     } else{
         alert('Incorrect PIN')
     }
-
-    const balance = document.getElementById('balance').innerText;
-    
-    const addMoneyNumber = parseFloat(addMoneyInput);
-    const balanceNumber = parseFloat(balance);
-
-    const newBalance = addMoneyNumber + balanceNumber;
-
-    document.getElementById('balance').innerText = newBalance;
-
-    console.log(newBalance);
 })
 
 document.getElementById('cash-out-btn').addEventListener('click', function(event){
@@ -34,11 +27,9 @@ document.getElementById('cash-out-btn').addEventListener('click', function(event
         const balance = document.getElementById('balance').innerText;
         const balanceNumber = parseFloat(balance);
         const newBalance = balanceNumber - cashOutNumber;
-
         document.getElementById('balance').innerText = newBalance;
+        document.getElementById('cash-out-input').value = '';
     } else{
         alert('Incorrect PIN, Please Try Again.')
     }
-
-
 })
--- JS/login.js
+++ JS/login.js
@@ -3,15 +3,11 @@ document.getElementById('login-btn').addEventListener('click', function(event){
 // Prevent browser reload 
     event.preventDefault();
 // Get the Phone Number and PIN Number.
-    // const phoneNumber = document.getElementById('phone-number').value;
+    const phoneNumber = document.getElementById('phone-number').value;
 
     const pinNumber = document.getElementById('pin-number').value;
 
-<<<<<<< HEAD
-    if (pinNumber === '1234'){
-=======
-    if (phoneNumber === '5' && pinNumber === '1234'){
->>>>>>> 8a43f5bfd66a1a6831c1dfc9753b401bd3da6b42
+    if (phoneNumber === '1234' && pinNumber === '1234'){
         window.location.href = './home.html';
     }
     else {
